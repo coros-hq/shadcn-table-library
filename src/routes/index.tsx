10e604fb79d432a21df91bc5d3683cdba262ed0b
oussamachah2020 <otaksam213@gmail.com>
2026-07-20 23:01:17 +0100

    WIP: continue from the example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,32 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { DataTable } from '@/components/example/data-table'
+import { columns } from '@/components/example/columns'
+import type { Payment } from '@/components/example/columns'
 
 export const Route = createFileRoute('/')({ component: Home })
 
-function Home() {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+
+async function Home() {
+  const data = await getData()
   return (
     <div>
       <h1>ShadCn Table Library</h1>
+      <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={data} />
+        </div>
     </div>
   )
 }
