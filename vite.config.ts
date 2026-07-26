91fb3d1f5ff7a228e093e5e600d12796b7542aea
oussamachah2020 <otaksam213@gmail.com>
2026-07-26 02:59:47 +0100

    adding nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,15 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+import { nitro } from 'nitro/vite';
 
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const config = defineConfig({
   resolve: {
     tsconfigPaths: true
   },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), nitro()],
   test: {
     projects: [{
       extends: true,
